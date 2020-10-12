c01c35ae52b13d69962792ed684855c5d8bd6168
Andie Grant <aegrant08@gmail.com>
2020-10-12 19:07:11 -0400

    added hour blocks to planner

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+
+    // set date and time on work planner
+    // date needs to update with day
+    // time needs to update
+    // display date in <p> on html page
+
+    // set up hour blocks
+    // set up rows for hour blocks
+    var $timeBlock = $("<div>");
+    $timeBlock.addClass("row");
+    $timeBlock.addClass("time-block");
+
+    // set hours in rows
+    // set up save buttons
+    // add floppy disc icon to save button
+    // tie hour blocks to date and time
+    // hour blocks that are past turn grey
+    // grab color styling from css stylesheet
+    // current hour block is red
+    // grab color styling from css stylesheet
+    // future hour blocks are green
+    // grab color styling from css stylesheet
+
+    // user can input text in hour blocks
+    // each hour block has a save button
+    // when saved button is pushed text is saved to local storage
+    // when page is refreshed input text in time blocks
+});
